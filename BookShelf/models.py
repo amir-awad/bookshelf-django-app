3dd14988d23ad06fe1335df3be50fda75a1ecef6
amir-awad <amirawad004@gmail.com>
2023-07-20 18:47:28 +0300

    created author model

--- BookShelf/models.py
+++ BookShelf/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 from django.utils import timezone
 
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    nationality = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 class Book(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    author = models.CharField(max_length=50)
+    author = models.ManyToManyField(Author)
 
     def __str__(self):
         return self.name
